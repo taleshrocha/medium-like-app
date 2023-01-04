6ac1956565726484f8d640f8d2343d44a6819806
taleshrocha <taleshrochaz@gmail.com>
2023-01-04 17:55:11 -0300

    Header done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,23 @@
 import Link from 'next/link'
 export default function Header() {
   return (
-    <header>
-      <div>
-        <Link>
+    <header className='flex justify-between p-5 max-w-7xl mx-auto'>
+      <div className='flex items-center space-x-5 p-5'>
+        <Link href='/'>
+          <img className='w-44 object-contain cursor-pointer'
+            src="https://links.papareact.com/yvf" alt="" />
         </Link>
+
+        <div className='hidden md:inline-flex items-center space-x-5'>
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>Follow</h3>
+        </div>
       </div>
-      <div>
+
+      <div className='flex items-center space-x-5 text-green-600'>
+        <h3>Sign In</h3>
+        <h3 className='border px-4 py-1 rounded-full border-green-600'>Get Started</h3>
       </div>
     </header>
   )
