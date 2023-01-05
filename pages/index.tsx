1486cc53ea8e1ceb25a5ca04ef34bc9757801a30
taleshrocha <taleshrochaz@gmail.com>
2023-01-05 15:22:03 -0300

    image handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,21 +35,21 @@ export default function Home({ posts }: Props) {
 
       {/* Posts */}
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
         {posts.map(post => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className=''>
+            <div className='group cursor-pointer border rounded-lg overflow-hidden'>
               {post.mainImage && (
-                <img src={
-                  urlFor(post.mainImage).url()!} alt="" />
+                <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
+                src={urlFor(post.mainImage).url()!} alt="" />
               )}
               <div className='flex justify-between p-5 bg-white'>
                 <div>
-                  <p>{post.title}</p>
+                  <p className='text-lg font-bold'>{post.title}</p>
                   <p>{post.description} (by {post.author.name})</p>
                 </div>
 
-                <img className='h-12 w-12 rounded-full'
+                <img className='h-12 w-12 rounded-full object-cover'
                 src={urlFor(post.author.image).url()!} alt="" />
               </div>
             </div>
