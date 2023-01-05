01222e9d2c5ef8b47a42fd40fcad82579706ffa2
taleshrocha <taleshrochaz@gmail.com>
2023-01-04 21:57:17 -0300

    Landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,27 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className='max-w-7xl mx-auto'>
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
+        <div className='px-10 space-y-5'>
+          <h1 className='text-6xl max-w-xl font-serif'>
+            <span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read and connect
+          </h1>
+          <h2>It's easy and free to post your thinking on any topic and connect with millions of readers.</h2>
+        </div>
+
+        <img
+          className='hidden md:inline-flex h-32 lg:h-full'
+          src='https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png'
+        />
+      </div>
     </div>
   )
 }
