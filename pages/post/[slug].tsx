5f531ac8cccff8f6e8a06e06297ed4051887ab1b
taleshrocha <taleshrochaz@gmail.com>
2023-01-06 15:04:22 -0300

    Comment interface

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -181,6 +181,22 @@ function Post({ post }: Props) {
             />
           </form>
         )}
+
+        {/* Comments */}
+
+        <div className="flex flex-col p-10 mt-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+          <h3 className="text-4xl">Comments</h3>
+          <hr className="pb-2" />
+
+          {post.comments.map((comment) => (
+            <div key={comment._id}>
+              <p>
+                <span className="text-yellow-500">{comment.name}: </span>
+                {comment.comment}
+              </p>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
