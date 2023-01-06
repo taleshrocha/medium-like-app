d79d36cecab25e5b22ed9e5430dd50b9a1c2b855
taleshrocha <taleshrochaz@gmail.com>
2023-01-06 17:36:26 -0300

    Fix on sanity

--- sanity-medium/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import comment from './comment'
-
-export const schemaTypes = [post, author, category, blockContent, comment]
